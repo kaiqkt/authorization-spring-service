1e79327293dc9250583ecddfc545ba1c759a7e51
kaiqkt <kaique..gomes@bandtec.com.br>
2021-05-18 11:28:42 -0300

    feature: removed unless classes

--- src/main/kotlin/authorizationservice/domain/repositories/RedisSessionRepository.kt
+++
@@ -1,9 +0,0 @@
-package authorizationservice.domain.repositories
-
-import authorizationservice.domain.entities.AuthSession
-
-interface RedisSessionRepository {
-    fun createSession(auth: AuthSession)
-    fun findSession(userId: String?): AuthSession?
-    fun deleteSession(userId: String?)
-}

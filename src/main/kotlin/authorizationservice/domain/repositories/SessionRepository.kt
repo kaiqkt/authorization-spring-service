b4c5ab540318eadc13b378a267203df0248247b2
Kaique Gomes <53584256+kaiqkt@users.noreply.github.com>
2021-05-24 19:07:02 -0300

    feat: created user logout (#28)
    Co-authored-by: kaiqkt <kaique..gomes@bandtec.com.br>

--- src/main/kotlin/authorizationservice/domain/repositories/SessionRepository.kt
+++ src/main/kotlin/authorizationservice/domain/repositories/SessionRepository.kt
@@ -1,10 +1,8 @@
 package authorizationservice.domain.repositories
 
-import authorizationservice.domain.entities.DeviceMetadata
 import authorizationservice.domain.entities.Session
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface SessionRepository: MongoRepository<Session, String> {
     fun findByUserId(userId: String?): List<Session>?
-    fun deleteByUserId(userId: String?)
 }
